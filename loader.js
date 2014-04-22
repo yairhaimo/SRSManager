870a28eeab40271fae0f8e42fd6ee6889e00eac6
yairhaimo <yair.haimo@gmail.com>
2014-04-22 16:30:49 +0300

    commit app

---
+++ loader.js
@@ -0,0 +1,22 @@
+﻿chrome.browserAction.onClicked.addListener(function (activeTab) {
+    var newURL = "index.html";
+    chrome.tabs.create({ url: newURL });
+});
+
+chrome.app.runtime.onLaunched.addListener(function (launchData) {
+    var requirementUrl = null;
+
+    if (launchData.id) {
+        // We are called to handle a URL that matches one of our url_handlers.
+        if (launchData.id === 'requirement') {
+            // Convert the desktop URL to a mobile one.
+            //requirementUrl = launchData.url.replace("en.wikipedia", "en.m.wikipedia");
+            console.log('got url:', launchData.url + ' for id:', launchData.id);
+        } else {
+            console.error("Unexpected URL handler ID: " + launchData.id);
+        }
+    } 
+
+    if (requirementUrl)
+        new AppWindow('chrome-extension://pcmpcnglkoldfegmpkgkcfdpddncampb/index.html#/details/-JL7qGVmyO6OPXad3KLj');
+});
